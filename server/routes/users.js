cdb0316b391335131c959bd360f1cf6e725e0b9b
Arnav D <arnav.deepaware@gmail.com>
2024-10-05 17:56:24 -0400

    Added update userByID api

--- server/routes/users.js
+++ server/routes/users.js
@@ -32,4 +32,26 @@ router.route('/:id').get((req, res) => {
         .catch(err => res.status(400).json('Error: ' + err));
 });
 
+//PUT Request to update a user by ID
+router.route('/update/:id').patch((req, res) => {
+    User.findById(req.params.id)
+        .then(user => {
+            if(!user){
+                return res.status(404).json('User Not Found');
+            }
+            
+            //Update the user attributes
+            user.username = req.body.username || user.username;
+            user.roadmaps = req.body.roadmaps || user.roadmaps;
+            user.skillsets = req.body.skillsets || user.skillsets;
+            user.careerpath = req.body.careerpath || user.careerpath;
+            user.recommendations = req.body.recommendations || user.recommendations;
+
+            user.save()
+                .then(() => res.json(`${user.username} updated!`))
+                .catch(err => res.status(400).json('Error: ' + err));
+        })
+})
+
+
 module.exports = router;
