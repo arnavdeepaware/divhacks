774801723a3813cd7e51e74da565e52de5adc8bd
Arnav D <arnav.deepaware@gmail.com>
2024-10-05 14:04:21 -0400

    Added mongoDB to our server file

--- server/index.js
+++ server/index.js
@@ -1,12 +1,23 @@
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
+
+require('dotenv').config();
+
 const app = express();
-const PORT = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-    res.send('Skill Builder App Backend');
-});
+app.use(cors());
+app.use(express.json());
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+const uri = process.env.ATLAS_URI;
+
+mongoose.connect(uri);
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("Mongo DB database connection successfully established");
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
